9dbbb146e8f7d61d272d0b25df3e6672ad5b4aa0
Pedro Tavares <pedro@pedrotavares.com>
2018-05-31 18:57:31 +0100

    Volume can now be updated by the user

--- js/VolumetricSnow.js
+++ js/VolumetricSnow.js
@@ -1,193 +1,304 @@
 'use strict';
 
-THREE.VolumetricSnow = function ( options ) {
+THREE.VolumetricParticles = function ( options ) {
 
-  THREE.Object3D.apply( this, arguments )
+  THREE.Object3D.apply( this, arguments );
 
-  options = options || {}
+  options = options || {};
 
 
+  this.clock = new THREE.Clock(); // NOTE: no influence at the moment
+  this.second = 0; // NOTE: no influence at the moment
 
-  // TODO: Add an emitter rate
-  // options.rate = options.rate || 100 // NOTE no influence at the moment
-  this.totalParticles = 0
-  this.maxParticles = options.maxParticles || 600
+
+  this.rate = options.rate || 1;
+  this.worldParticles = new THREE.Group();
+  this.totalParticles = 0;
+  this.maxParticles = options.maxParticles || 500;
   // TODO: Change age to be in seconds but still only count down in frames
-  this.maxAge = options.maxAge || 600 // in ticks / frames
-  this.volume = options.volume || { w:15, h:15, d:15 }
-  this.sprite = options.sprite || './textures/particle2.png'
+  this.maxAge = options.maxAge || 6;
+  this.volume = options.volume || { w:1, h:1, d:1 };
+  this.sprite = options.sprite || './textures/particle2.png';
 
 
 
-  const s_map = new THREE.TextureLoader().load( this.sprite )
-  // TODO: Add a toggle to use either a sprite or mesh
-  // NOTE: Sprites do not show through other meshes, even transparent ones
-  // this.s_mat = new THREE.SpriteMaterial( { map:s_map, color:0xffffff, transparent:true })
-  this.s_mat = new THREE.MeshBasicMaterial( { map:s_map, color:0xffffff, transparent:true })
-
-  this.s_geo = new THREE.CircleBufferGeometry( 0.2, 3 )
+  const s_map = new THREE.TextureLoader().load( this.sprite );
+  // NOTE: Sprites seem not show through other meshes, even transparent ones
+  this.s_mat = new THREE.SpriteMaterial( { map:s_map, color:0xffffff, transparent:true });
 
 
 
-  this.random = function (min, max) {
+  this.random = function ( min, max ) {
     // return a number between two boundaries
     return Math.random() * ( max - min ) + min;
   }
 
 
 
-  // TODO: Rename this to refer to the volume of the emitter
-  this.randomVec3 = function (min, max) {
-    // TODO: return a Vector3 out from the precalculated list
+  this.getPointWithinVolume = function () {
+
+    // TODO: maybe return a Vector3 out from the precalculated list
     // console.log('>>>', this.volume);
-    return new THREE.Vector3( this.random(-this.volume.w*0.5,this.volume.w*0.5), this.random(-this.volume.h*0.5,this.volume.h*0.5), this.random(-this.volume.d*0.5,this.volume.d*0.5) )
+    return new THREE.Vector3(
+      this.random( -this.volume.w * 0.5, this.volume.w * 0.5 ),
+      this.random( -this.volume.h * 0.5, this.volume.h * 0.5 ),
+      this.random( -this.volume.d * 0.5, this.volume.d * 0.5 )
+    );
+
   }
 
-
+  // console.log(this.parent, this);
 
   // TODO: Separate newParticle contents from the emitter
-  this.newParticle = function () {
+  // this.newParticle = function () {
+  //
+  //   this.totalParticles++;
+  //
+  //   if( this.totalParticles >= this.maxParticles ) {
+  //     console.log('enough particles', this);
+  //     const that = this;
+  //     clearInterval( this.interval );
+  //   }
+  //
+  //   // TODO: Rename this to relate to the volume of the emitter
+  //   // const vec3 = this.randomVec3()
+  //
+  //   // TODO: Add a toggle to use either a sprite or mesh
+  //   // NOTE: Sprites do not show through other meshes, even transparent ones
+  //   // const sprite = new THREE.Sprite( this.s_mat )
+  //   const sprite = new THREE.Mesh( this.s_geo, this.s_mat );
+  //   sprite.position.set( vec3.x, vec3.y, vec3.z );
+  //
+  //
+  //   sprite.userData.acceleration = {
+  //     x:0,
+  //     y:0,
+  //     z:0
+  //   };
+  //
+  //   // TODO try to have less calls to the random method. Maybe precalculate them and store
+  //   sprite.userData.velocity = {
+  //     x:this.random( -0.1, 0.1 ),
+  //     y:this.random( -0.01, -0.1 ),
+  //     z:0
+  //   }
+  //   sprite.userData.age = 0
+  //   // we're counting age on frame ticks
+  //   sprite.userData.maxAge = this.maxAge // If maxAge is greater than maxParticles, we get a gap
+  //
+  //
+  //   // NOTE: Don't automatically recycle the particle, just kill it
+  //   sprite.userData.reset = function () {
+  //
+  //     // TODO: Rename this to relate to the volume of the emitter
+  //     const _v3 = sprite.parent.randomVec3();
+  //     // console.log('reset', sprite.userData);
+  //     sprite.position.set( _v3.x, _v3.y, _v3.z );
+  //     sprite.userData.age = 0;
+  //
+  //     sprite.userData.velocity = {
+  //       x:sprite.parent.random( -0.1, 0.1 ),
+  //       y:sprite.parent.random( -0.01, -0.1 ),
+  //       z:0
+  //     };
+  //
+  //   }
+  //
+  //
+  //   sprite.userData.tick = function ( time ) {
+  //     // console.log('tick', this);
+  //     sprite.userData.age++
+  //
+  //
+  //     sprite.userData.acceleration.y = sprite.parent.random( -0.001, 0.001 );
+  //     // TODO: Change x movement to use sin/cos
+  //     sprite.userData.acceleration.x = sprite.parent.random( -0.01, 0.01 );
+  //
+  //
+  //     sprite.userData.velocity.y = sprite.userData.velocity.y + sprite.userData.acceleration.y;
+  //     sprite.userData.velocity.x = sprite.userData.velocity.x + sprite.userData.acceleration.x;
+  //
+  //
+  //     sprite.position.y = sprite.position.y + sprite.userData.velocity.y;
+  //     sprite.position.x = sprite.position.x + sprite.userData.velocity.x;
+  //
+  //
+  //     if( sprite.userData.age >= sprite.userData.maxAge ) {
+  //       // console.log('Die!', sprite.parent);
+  //       // sprite.parent.remove( sprite )
+  //       sprite.userData.reset();
+  //
+  //     }
+  //   }
+  //
+  //   // console.log(sprite);
+  //
+  //   // TODO: Toggle to lock/unlock the particles relative to the emitter
+  //   this.add( sprite )
+  //
+  // },
 
-    // TODO: Rename this to relate to the volume of the emitter
-    const vec3 = this.randomVec3()
-
-    // TODO: Add a toggle to use either a sprite or mesh
-    // NOTE: Sprites do not show through other meshes, even transparent ones
-    // const sprite = new THREE.Sprite( this.s_mat )
-    const sprite = new THREE.Mesh( this.s_geo, this.s_mat )
-    sprite.position.set( vec3.x, vec3.y, vec3.z )
-
-
-    sprite.userData.acceleration = {
-      x:0,
-      y:0,
-      z:0
-    }
-
-    // TODO try to have less calls to the random method. Maybe precalculate them and store
-    sprite.userData.velocity = {
-      x:this.random(-0.1,0.1),
-      y:this.random(-0.01,-0.1),
-      z:0
-    }
-    sprite.userData.age = 0
-    // we're counting age on frame ticks
-    sprite.userData.maxAge = this.maxAge // If maxAge is greater than maxParticles, we get a gap
 
 
 
-    sprite.userData.reset = function () {
+  this.spawn = function () {
 
-      // TODO: Rename this to relate to the volume of the emitter
-      const _v3 = sprite.parent.randomVec3()
-      // console.log('reset', sprite.userData);
-      sprite.position.set( _v3.x, _v3.y, _v3.z )
-      sprite.userData.age = 0;
+    for (var i = 0; i < this.rate; i++) {
 
-      sprite.userData.velocity = {
-        x:sprite.parent.random(-0.1,0.1),
-        y:sprite.parent.random(-0.01,-0.1),
-        z:0
+      if( this.totalParticles < this.maxParticles ) {
+
+        const p = new THREE.VolumetricParticle(
+          {
+            material: this.s_mat,
+            volume: this.volume,
+            position: this.getPointWithinVolume(),
+            age: this.maxAge
+          }
+        );
+
+        this.add( p );
+
+        ++this.totalParticles;
+
+      } else {
+
+        break;
+
       }
 
     }
 
-
-    sprite.userData.tick = function ( time ) {
-      // console.log('tick', this);
-      sprite.userData.age++
+  },
 
 
-      sprite.userData.acceleration.y = sprite.parent.random(-0.001, 0.001)
-      // TODO: Change x movement to use sin/cos
-      sprite.userData.acceleration.x = sprite.parent.random(-0.01, 0.01)
 
+  this.dispose = function ( particle ) {
 
-      sprite.userData.velocity.y = sprite.userData.velocity.y + sprite.userData.acceleration.y
+    this.remove( particle );
 
-      sprite.userData.velocity.x = sprite.userData.velocity.x + sprite.userData.acceleration.x
+    --this.totalParticles;
 
-
-      sprite.position.y = sprite.position.y + sprite.userData.velocity.y
-      sprite.position.x = sprite.position.x + sprite.userData.velocity.x
-
-
-      if(sprite.userData.age >= sprite.userData.maxAge) {
-        // console.log('Die!', sprite.parent);
-        // sprite.parent.remove( sprite )
-        sprite.userData.reset()
-
-      }
-    }
-
-    // console.log(sprite);
-
-    // TODO: Toggle to lock/unlock the particles relative to the emitter
-    this.add( sprite )
-
-  }
-
+  },
 
 
 
 
   this.update = function () {
 
-    // TODO: Enable emitter rate per frame
-    if(this.totalParticles < this.maxParticles) {
-      // console.log('[VolumetricSnow] update', this.totalParticles, this.maxParticles);
-      this.newParticle()
-      this.totalParticles++;
-    }
+    this.spawn();
 
+    this.children.forEach( child => {
 
-    this.children.forEach(function (child) {
-      if(child.userData.tick) {
-        child.userData.tick()
-      }
-    })
+      if( child.update ) {
 
-  }
+        child.update(
+          new THREE.Vector3(
+            this.random( -0.005, 0.005 ),
+            this.random( -0.0015, 0.001),
+            this.random( -0.005, 0.005 )
+          )
+        );
 
+      };
 
-
-
-
-  this.resize = function (newVolume) {
-    // NOTE: si there a better way to calculate this
-    if(this.visualVolume) {
-      this.visualVolume.scale.x = newVolume.w / this.volume.w
-      this.visualVolume.scale.y = newVolume.h / this.volume.h
-      this.visualVolume.scale.z = newVolume.d / this.volume.d
-    }
-
-
-    this.volume = newVolume
+    });
 
   }
 
+
+
+  this.resize = function ( newVolume ) {
+
+    this.volume = newVolume;
+
+    if( this.visualVolume ) {
+
+      this.remove( this.visualVolume );
+
+      this.addHelper( this.helperColor );
+
+    }
+
+  }
 
 
 
   // Create a visual helper
-  this.addHelper = function (color) {
+  this.addHelper = function ( color ) {
 
-    const _color = color || 0xFF00FF
+    this.helperColor = color || 0xFF00FF;
 
-    const v_geo = new THREE.BoxBufferGeometry( this.volume.w, this.volume.h, this.volume.d )
-    const v_mat = new THREE.MeshBasicMaterial( {color:_color, wireframe:true} )
+    const v_geo = new THREE.BoxBufferGeometry( this.volume.w, this.volume.h, this.volume.d );
+    const v_mat = new THREE.MeshBasicMaterial( { color:this.helperColor, wireframe:true } );
 
+    this.visualVolume = new THREE.Mesh( v_geo, v_mat );
 
-    this.visualVolume = new THREE.Mesh( v_geo, v_mat )
-
-    this.add( this.visualVolume )
+    this.add( this.visualVolume );
 
   }
 
 }
 
 
-
 // Define the prototype and constructor to be THREE's prototype and constructor
-THREE.VolumetricSnow.prototype = Object.create( THREE.Object3D.prototype )
-THREE.VolumetricSnow.prototype.constructor = THREE.VolumetricSnow
+THREE.VolumetricParticles.prototype = Object.create( THREE.Object3D.prototype );
+THREE.VolumetricParticles.prototype.constructor = THREE.VolumetricParticles;
+
+
+
+
+
+
+THREE.VolumetricParticle = function ( options ) {
+
+  THREE.Object3D.apply( this, arguments );
+
+  if(!options) {
+    console.error("Please provide a set of options");
+    return;
+  }
+
+  this.add( new THREE.Sprite( options.material ) );
+
+  // console.log('volumetricParticles', options.pos || new THREE.Vector3());
+  this.position.set( options.position.x, options.position.y, options.position.z );
+  this.velocity = options.vel || new THREE.Vector3();
+  this.acceleration = options.acc || new THREE.Vector3(0, -0.0025, 0);
+  this.age = ( options.age || 3 ) * 60; // expected fps is 60
+
+
+  this.update = function ( vector3 ) {
+
+    if( this.age === 0 ) {
+
+      this.die();
+
+      return;
+
+    }
+
+    if( vector3 ) {
+      this.acceleration = vector3;
+    }
+
+    this.velocity.add( this.acceleration );
+    this.position.add( this.velocity );
+
+    this.age--;
+  }
+
+
+
+  this.die = function () {
+
+    this.parent.dispose( this );
+
+  }
+
+  return this;
+
+}
+
+
+THREE.VolumetricParticle.prototype = Object.create( THREE.Object3D.prototype )
+THREE.VolumetricParticle.prototype.constructor = THREE.VolumetricParticle
--- snow.js
+++ snow.js
@@ -1,114 +1,159 @@
-(function () {
-
-
-  var target = document.querySelector( 'body' )
-
-
-
-  window.addEventListener('resize', resize)
-
-
-  var scene = new THREE.Scene(),
-      renderer = new THREE.WebGLRenderer({canvas:canvas}),
-      camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 150)
+window.addEventListener('resize', resize);
 
 
 
 
 
-  renderer.setPixelRatio( window.devicePixelRatio )
-  renderer.setSize( window.innerWidth, window.innerHeight )
+var target = document.querySelector('body');
 
 
 
 
 
-  camera.position.y = 10
-  camera.position.z = 50
-  camera.lookAt(new THREE.Vector3())
+var renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 
 
 
 
-  var gridHelper = new THREE.GridHelper(400, 40, 0x0000ff, 0x808080)
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
 
-  scene.add(gridHelper)
+camera.position.y = 10;
+camera.position.z = 50;
+camera.lookAt(new THREE.Vector3());
 
 
 
 
-
-  var volumetricSnow = new THREE.VolumetricSnow({
-    volume:{
-      w:25,
-      h:10,
-      d:25
-    },
-    maxParticles:1,
-    maxAge: 100
-  })
-
-  volumetricSnow.position.y = 10
-
-  // Helper to visualise where the snowflakes are being created
-  volumetricSnow.addHelper(0x00FF00) // the color argument is optional. Only takes hexadecimal numbers
-
-  scene.add( volumetricSnow )
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 
 
 
 
-  // One can resposition and resize the area of the emmiter
-  // just note that all particles will move with the volume area of the emitter
-  // setTimeout(function () {
-  //   volumetricSnow.resize({
-  //     w:1,
-  //     h:1,
-  //     d:1
-  //   })
-  //
-  //   // volumetricSnow.position.x = -15
-  //   // volumetricSnow.position.z = -150
-  //
-  // }, 3000)
+var scene = new THREE.Scene();
 
 
 
-  function resize() {
 
-    console.log('resize');
+var gridHelper = new THREE.GridHelper(400, 40, 0x0000ff, 0x808080);
 
-    camera.aspect = window.innerWidth /window.innerHeight
-    camera.updateProjectionMatrix()
+scene.add(gridHelper);
 
 
-    renderer.setSize( window.innerWidth, window.innerHeight )
 
+var initialVolume = {
+  w: 16,
+  h: 3,
+  d: 16
+}
+
+var volumetricParticles = new THREE.VolumetricParticles({
+  volume: initialVolume,
+  // maxParticles:150,
+  // maxAge: 1,
+  // rate: 1
+});
+
+volumetricParticles.position.y = 18;
+
+// Helper to visualise where the snowflakes are being created
+volumetricParticles.addHelper(0x00FF00); // the color argument is optional. Only takes hexadecimal numbers
+
+scene.add(volumetricParticles);
+
+
+
+// One can resposition and resize the area of the emmiter
+// just note that all particles will move with the volume area of the emitter
+// setInterval(function () {
+//   console.log("interval");
+//   volumetricParticles.resize({
+//     w:1,
+//     h:1,
+//     d:1
+//   });
+// }, 3000);
+
+
+
+function resize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+
+
+
+
+function render() {
+
+  requestAnimationFrame(render);
+
+  volumetricParticles.update();
+
+  renderer.render(scene, camera);
+
+}
+
+
+
+
+
+function onInputChange(e) {
+
+  // Only run if we have a valid updated value
+  if (!this.value) return;
+
+  var newVolume = {
+    w: initialVolume.w,
+    h: initialVolume.h,
+    d: initialVolume.d
   }
 
+  inputs.forEach(function(input) {
+
+    var value = Number(input.value);
+
+    // only run if the current input has a value
+    if (!value || value <= 0) return;
+
+    switch (input.name) {
+      case "height":
+        newVolume.h = value;
+        break;
+      case "width":
+        newVolume.w = value;
+        break;
+      case "depth":
+        newVolume.d = value;
+        break;
+      default:
+    }
+  });
+
+  volumetricParticles.resize(newVolume);
+}
 
 
 
 
-  function render() {
 
-    volumetricSnow.update()
+var inputs = document.querySelectorAll('input');
 
-
-    renderer.render(scene, camera)
-
-    requestAnimationFrame(render)
-
-  }
+inputs.forEach(function(input) {
+  input.addEventListener('blur', onInputChange);
+});
 
 
-
-
-  // Kick off
-  render();
-
-
-
-})();
+// Kick off
+render();
